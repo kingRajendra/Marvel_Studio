3c03dab776174c916191119469d0f7bc966f22fa
rajendra1795 <rprasad@developerscode.com>
2023-01-19 22:52:58 +0530

    Marvel

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
-        <h1>Hello</h1>
+        <h1>Marvel</h1>
       </header>
     </div>
   );
